ffcaca807420cda473b55306ce103a197827b782
Beyond the Aquila Rift <104922779+danielchen2003@users.noreply.github.com>
2023-07-14 11:11:18 -0400

    comment

--- pratice072023/leet70.js
+++ pratice072023/leet70.js
@@ -21,7 +21,34 @@ var reverseList = function(head) {
         cur.next = pre 
         return helper(next,cur)
     }
-    return helper(cur,null)
+    return helper(head,null)
 };
 
 
+var reverseList = function(head) {
+    let pre = null 
+    let cur = head 
+    while(cur){
+        let next = cur.next 
+        cur.next = pre 
+        pre = cur 
+        cur = next
+    }
+    return pre
+}
+
+
+
+var reverseList = function(head) {
+    if(!head) return head 
+    
+    function helper(pre,cur){
+        if(!cur) return pre
+        let next = cur.next
+        cur.next = pre
+        return helper(cur,next)
+        
+    }
+    
+    return helper(null,head)
+}
